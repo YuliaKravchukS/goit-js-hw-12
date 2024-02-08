cc797f001428dd6e634f8b2958ef2bb12ef0a9e5
YuliaKravchukS <ljuljek554@gmail.com>
2024-02-08 12:31:28 +0200

    add btn load

---
+++ src/main.js
@@ -0,0 +1,123 @@
+'use strict'
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Axios from "axios";
+
+
+const refs = {
+    formEl: document.querySelector('.form'),
+    input: document.querySelector('#valueGallery'),
+    galleryEl: document.querySelector('.gallery'),
+    loader: document.querySelector('.loader'),
+    btnLoad: document.querySelector('.btn-load'),
+    
+}
+
+let page = 1;
+const limit = 15;
+const totalPages = Math.ceil(500 / limit);
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.loader.style.visibility = 'hidden';
+
+
+async function onFormSubmit(e) {
+    e.preventDefault();
+    refs.galleryEl.innerHTML = '';
+    const value = e.target.elements.valueGallery.value;
+    if (value !== '') {
+        refs.loader.style.visibility = 'visible';
+    }        
+    
+    const data = await getUrl(value);
+        if (data.length > 0 & value !== '') {
+            refs.loader.style.visibility = 'hidden';
+            renderImages(data);
+            page += 1;
+            
+            if (page > 1) {
+                refs.btnLoad.style.visibility = 'visible';
+                refs.btnLoad.addEventListener('click', async () => {
+                    const data = await getUrl(value);
+                    renderImages(data);
+                    page += 1;
+                });
+            }if (condition) {
+                
+            } else {
+                
+            }
+            value.addEventListener('change', () => {
+                    refs.btnLoad.style.visibility = 'hidden'; 
+                    page = 1;
+               });
+            
+        } else {
+            refs.loader.style.visibility = 'hidden';
+            iziToast.error({
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight',
+            })
+        }
+    
+        
+    e.target.reset();
+};
+
+
+const axios = Axios.create({
+    baseURL: 'https://pixabay.com/api/',
+    
+})
+async function getUrl(clientValue) {
+
+    const response =await axios.get('',{params: {
+        key: '42132466-2eec74b8e2a534f613ea758a4',
+        q: clientValue,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: limit,
+        page: page,
+        
+        
+    }});
+    return response.data.hits;
+
+}
+
+
+
+function imageTemplate(img) {
+    const {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = img;
+    
+    return `<li>
+    <div class="card">
+    <div class="img-container">
+    <a href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" />
+    </a>
+    </div>
+    <div class="img-comments">
+    <p class="describe">Likes ${ likes }</p>
+    <p class="describe">Views ${ views }</p>
+    <p class="describe">Comments ${ comments }</p>
+    <p class="describe">Downloads ${downloads}</p>
+    </div>
+    </div>
+    </li>
+    `;
+}
+
+
+function imagesTemplate(images) {
+    return images.map(imageTemplate).join('');
+}
+function renderImages(images) {
+    const markup = imagesTemplate(images) ;
+    refs.galleryEl.insertAdjacentHTML('beforeend',markup);
+    let simpleLightBox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 }).refresh();
+};
